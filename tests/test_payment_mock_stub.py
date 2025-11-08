a476c24091018b2b5b5d8e8988e5f34a8533ff27
Nathan Daneliak <nadandevelop@gmail.com>
2025-11-08 17:08:16 -0500

    Added new tests for coverage

--- tests/test_payment_mock_stub.py
+++ tests/test_payment_mock_stub.py
@@ -11,7 +11,7 @@ from services.payment_service import (
 def test_pay_late_fees_valid(mocker):
     """Test a valid payment by mocking success of the process_payment function and entering correct values"""
     mocker.patch("services.library_service.calculate_late_fee_for_book", return_value = {"fee_amount": 10.0}) # Stubs for separate calls
-    mocker.patch("database.get_book_by_id", return_value = {"title": "The Great Gatsby"})
+    mocker.patch("services.library_service.get_book_by_id", return_value = {"title": "The Great Gatsby"})
 
     mock_gateway = Mock(spec=PaymentGateway)
     mock_gateway.process_payment.return_value = (True, "txn_123", "Success")
@@ -28,7 +28,7 @@ def test_pay_late_fees_valid(mocker):
 def test_pay_late_fees_declined(mocker):
     """Test an invalid payment by mocking failure of the process_payment function"""
     mocker.patch("services.library_service.calculate_late_fee_for_book", return_value = {"fee_amount": 10.0})
-    mocker.patch("database.get_book_by_id", return_value = {"title": "The Great Gatsby"})
+    mocker.patch("services.library_service.get_book_by_id", return_value = {"title": "The Great Gatsby"})
 
     mock_gateway = Mock(spec=PaymentGateway)
     mock_gateway.process_payment.return_value = (False, "", "Declined By Gateway")
@@ -44,7 +44,7 @@ def test_pay_late_fees_declined(mocker):
 def test_pay_late_fees_invalid_patronid(mocker):
     """Test pay_late_fees with an invalid patron id (failure)"""
     mocker.patch("services.library_service.calculate_late_fee_for_book", return_value = {"fee_amount": 10.0})
-    mocker.patch("database.get_book_by_id", return_value = {"title": "The Great Gatsby"})
+    mocker.patch("services.library_service.get_book_by_id", return_value = {"title": "The Great Gatsby"})
 
     mock_gateway = Mock(spec=PaymentGateway)
     success, msg, txn = pay_late_fees("12345", 1, mock_gateway)
@@ -59,7 +59,7 @@ def test_pay_late_fees_invalid_patronid(mocker):
 def test_pay_late_fees_zero_late_fee(mocker):
     """Test pay_late_fees failure by stubbing the late fee calculation to 0"""
     mocker.patch("services.library_service.calculate_late_fee_for_book", return_value = {"fee_amount": 0.0})
-    mocker.patch("database.get_book_by_id", return_value = {"title": "The Great Gatsby"})
+    mocker.patch("services.library_service.get_book_by_id", return_value = {"title": "The Great Gatsby"})
 
     mock_gateway = Mock(spec=PaymentGateway)
     success, msg, txn = pay_late_fees("123456", 1, mock_gateway)
@@ -73,7 +73,7 @@ def test_pay_late_fees_zero_late_fee(mocker):
 def test_pay_late_fees_network_error(mocker):
     """Test a network error by mocking an error on process_payment"""
     mocker.patch("services.library_service.calculate_late_fee_for_book", return_value = {"fee_amount": 10.0})
-    mocker.patch("database.get_book_by_id", return_value = {"title": "The Great Gatsby"})
+    mocker.patch("services.library_service.get_book_by_id", return_value = {"title": "The Great Gatsby"})
 
     mock_gateway = Mock(spec=PaymentGateway)
     mock_gateway.process_payment.return_value = Exception("Bad Network")
@@ -85,6 +85,35 @@ def test_pay_late_fees_network_error(mocker):
 
     mock_gateway.process_payment.assert_called_once()
     mock_gateway.process_payment.assert_called_with(patron_id = "123456", amount = 10.0, description = "Late fees for 'The Great Gatsby'")
+
+# Additional Tests to complete statement coverage:
+def test_pay_late_fees_invalid_fee_info(mocker):
+    """Test pay_late_fees failure where calculate_late_fee_for_book fails"""
+    mocker.patch("services.library_service.calculate_late_fee_for_book", return_value = {"fee_not_amount": 10.0})
+
+    mock_gateway = Mock(spec=PaymentGateway)
+    success, msg, txn = pay_late_fees("123456", 1, mock_gateway)
+
+    assert success is False
+    assert "Unable to calculate" in msg
+    assert txn is None
+
+    mock_gateway.process_payment.assert_not_called()
+
+def test_pay_late_fees_invalid_no_book(mocker):
+    """Test pay_late_fees failure where the payment gateway is None"""
+    mocker.patch("services.library_service.calculate_late_fee_for_book", return_value = {"fee_amount": 10.0})
+    mocker.patch("services.library_service.get_book_by_id", return_value = None)
+
+    mock_gateway = Mock(spec=PaymentGateway)
+    success, msg, txn = pay_late_fees("123456", 1, None)
+
+    assert success is False
+    assert "Book not found" in msg
+    assert txn is None
+
+    mock_gateway.process_payment.assert_not_called()
+
 
 
 def test_refund_late_fee_valid():
@@ -140,3 +169,29 @@ def test_refund_late_fee_exceed_max_amount():
     assert "maximum late fee" in msg
 
     mock_gateway.refund_payment.assert_not_called()
+
+    # Additional tests to complete statement coverage
+def test_refund_late_fee_error():
+    """Test an error by mocking an error on refund_payment"""
+    mock_gateway = Mock(spec=PaymentGateway)
+    mock_gateway.refund_payment.return_value = Exception("Error")
+    success, msg = refund_late_fee_payment("txn_123", 10.0, mock_gateway)
+
+    assert success is False
+    assert "Refund processing error" in msg
+
+
+    mock_gateway.refund_payment.assert_called_once()
+    mock_gateway.refund_payment.assert_called_with("txn_123", 10.0)
+
+def test_refund_late_fee_declined():
+    """Test an invalid refund by mocking failure of the refund_payment function"""
+    mock_gateway = Mock(spec=PaymentGateway)
+    mock_gateway.refund_payment.return_value = (False, "Declined By Gateway")
+    success, msg = refund_late_fee_payment("txn_123", 10.0, mock_gateway)
+
+    assert success is False
+    assert "Declined By Gateway" in msg
+
+    mock_gateway.refund_payment.assert_called_once()
+    mock_gateway.refund_payment.assert_called_with("txn_123", 10.0)
